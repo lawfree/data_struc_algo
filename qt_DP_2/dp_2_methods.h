6b25fd1039faea598dbd35255616996deb585f3f
lowfree <745363151@qq.com>
2019-07-23 09:02:03 +0800

    Minimum Path Sum and Dungeon Game:  to complete

---
+++ qt_DP_2/dp_2_methods.h
@@ -0,0 +1,18 @@
+#ifndef DP_2_METHODS_H
+#define DP_2_METHODS_H
+
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+int makeup_money();
+
+int triangle_min_path();
+
+int grid_min_path();
+
+int dungeon_game();
+
+#endif // DP_2_METHODS_H
---
+++ qt_DP_2/main.cpp
@@ -0,0 +1,13 @@
+#include "dp_2_methods.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    //makeup_money();
+    triangle_min_path();
+
+
+    return 0;
+}
