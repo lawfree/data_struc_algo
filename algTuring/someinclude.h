7dff0c60c26f7e5bcf076e2d00564942d5e06651
lowfree <745363151@qq.com>
2019-08-23 17:06:44 +0800

    algTuring

---
+++ algTuring/someinclude.h
@@ -0,0 +1,12 @@
+#ifndef SOMEINCLUDE_H
+#define SOMEINCLUDE_H
+#include<stdio.h>
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std ;
+
+#endif
